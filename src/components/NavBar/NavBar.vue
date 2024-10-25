5be4e1f28527ef0119c8b4bf2759da09b8b6f11d
manuelaperezsimon <manuela@swipoo.com>
2024-10-25 13:08:45 +0200

    create Spinner, navBar and Yacht components

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="max-w-6xl mx-auto py-6">
+    <h1 class="font-frank text-center text-3xl mb-10 text-gray-800 font-light">
+      ROY
+    </h1>
+
+    <header
+      class="flex justify-between items-center border-b border-gray-200 pb-2"
+    >
+      <div>
+        <h2 class="text-lg text-gray-800 font-light font-urbanBold">
+          YACHTS FOR SALE · {{ totalYachts }}
+        </h2>
+      </div>
+
+      <div
+        class="flex items-center space-x-4 text-gray-600 font-light font-urbanMedium"
+      >
+        <span class="text-gray-500">| </span>
+
+        <span class="text-gray-500">View</span>
+
+        <button
+          @click="$emit('toggleView', true)"
+          class="p-1 border border-gray-300 rounded hover:bg-gray-200 focus:outline-none"
+          :class="{ 'bg-gray-300': isGrid }"
+          aria-label="Grid View"
+        >
+          <i class="fas fa-th"></i>
+        </button>
+
+        <button
+          @click="$emit('toggleView', false)"
+          class="p-1 border border-gray-300 rounded hover:bg-gray-200 focus:outline-none"
+          :class="{ 'bg-gray-300': !isGrid }"
+          aria-label="List View"
+        >
+          <i class="fas fa-th-list"></i>
+        </button>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ totalYachts: number; isGrid: boolean }>();
+</script>
--- src/components/Yacht/YachtCard.vue
+++ src/components/Yacht/YachtCard.vue
@@ -1,28 +1,30 @@
 <template>
   <div
-    class="bg-white shadow-lg border border-gray-200 rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300"
+    class="bg-white overflow-hidden hover:shadow-xl transition-shadow duration-300"
   >
     <img
       :src="yacht.coverImage.url"
-      alt="Yacht image"
-      class="w-full h-64 object-cover rounded-t-lg"
+      :alt="yacht.name"
+      class="w-full h-64 object-cover"
     />
     <div class="">
-      <p class="text-lg font-urbanGrotesk text-gray-400">
-        Price: {{ yacht.buyPrice.EUR }} EUR
+      <p class="text-sm font-urbanMedium font-light text-gray-400 mb-1 mt-2">
+        Price: €{{ yacht.buyPrice.EUR.toLocaleString("de-DE") }}
       </p>
-      <p class="text-sm text-gray-600">
+      <p class="text-sm text-gray-600 font-light font-urbanMedium">
         Length: {{ yacht.length.meters }}m | Guests: {{ yacht.guestsNumber }} |
         Cabins: {{ yacht.cabinsNumber }}
       </p>
-      <h2 class="text-2xl font-bold mt-4 text-gray-800">{{ yacht.name }}</h2>
+      <h2 class="text-xl mt-4 text-gray-800 font-light font-urbanBold">
+        {{ yacht.name }}
+      </h2>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { defineProps } from "vue";
-import type { Yacht } from "~/src/interfaces/yachts";
+import type { Yacht } from "../../interfaces/yachts";
 
 const props = defineProps<{ yacht: Yacht }>();
 </script>
